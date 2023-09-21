a4584cbebe330573aa13d41951aca0e71830f41e
Manny <parra.manny@gmail.com>
2023-09-20 20:05:38 -0600

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,12 @@
 'use client'
 import styles from './page.module.css'
 import ShareBioAge from '@/components/ShareBioAge'
-import BioAge from '@/components/BioAge'
-import TestDownload from '@/components/TestDownload'
-import ShareBioAgeV2 from '@/components/ShareBioAgeV2'
 
 
 export default function Home() {
   return (
       <main className={styles.main}>
-        {/*<ShareBioAge />*/}
-        <ShareBioAgeV2 />
-        {/*<TestDownload/>*/}
+        <ShareBioAge />
       </main>
   )
 }
--- src/components/ShareBioAge.js
+++ src/components/ShareBioAge.js
@@ -1,72 +1,109 @@
-// import React, { useState } from 'react';
-// import BioAge from './BioAge';
-// import domtoimage from 'dom-to-image';
-//
-// function ShareBioAge() {
-//   const [sharing, setSharing] = useState(false);
-//   const [shareError, setShareError] = useState(null);
-//
-//   const handleShareChart = () => {
-//     setSharing(true);
-//     const bioAgeComponent = document.querySelector('.bio-age-container');
-//
-//     if (bioAgeComponent) {
-//       domtoimage.toJpeg(bioAgeComponent, {
-//         bgcolor: 'white',
-//       })
-//           .then((dataUrl) => {
-//             if (navigator.share) {
-//               // Share the chart image using Web Share API
-//               navigator.share({
-//                 text: 'Biological Age Chart Test',
-//                 files: [new File([dataUrl], 'biological-age.jpeg', { type: 'image/jpeg' })],
-//               }).catch((error) => {
-//                 console.error('Error sharing BioAge component:', error);
-//               });
-//             } else {
-//               // Fallback for browsers that don't support Web Share API
-//               // Create a temporary anchor element for downloading the image
-//               const downloadLink = document.createElement('a');
-//               downloadLink.href = dataUrl;
-//               downloadLink.download = 'biological-age-chart.jpeg';
-//               downloadLink.style.display = 'none';
-//
-//               // Append the anchor element to the document body
-//               document.body.appendChild(downloadLink);
-//
-//               // Trigger a click event on the anchor to initiate the download
-//               downloadLink.click();
-//
-//               // Remove the anchor element from the DOM
-//               document.body.removeChild(downloadLink);
-//
-//               alert('Chart image downloaded for manual sharing.');
-//             }
-//             setSharing(false);
-//           })
-//           .catch((error) => {
-//             console.error('Error capturing BioAge component:', error);
-//             setShareError('Error capturing the chart image.');
-//             setSharing(false);
-//           });
-//     } else {
-//       setShareError('Error: The BioAge component could not be found.');
-//       setSharing(false);
-//     }
-//   };
-//
-//   return (
-//       <div>
-//         <div className="bio-age-container">
-//           <BioAge />
-//         </div>
-//         {shareError && <div className="error">{shareError}</div>}
-//         <button onClick={handleShareChart} disabled={sharing}>
-//           {sharing ? 'Sharing...' : 'Share Biological Age Chart'}
-//         </button>
-//         {shareError && <div className="error">{shareError}</div>}
-//       </div>
-//   );
-// }
-//
-// export default ShareBioAge;
+import React, { useState } from 'react';
+import BioAge from './BioAge';
+import domtoimage from 'dom-to-image';
+
+function ShareBioAge() {
+  const [sharing, setSharing] = useState(false);
+  const [shareError, setShareError] = useState(null);
+
+  const handleShareChart = async () => {
+    setSharing(true);
+    const bioAgeComponent = document.querySelector('.bio-age-container');
+
+    if (bioAgeComponent) {
+      try {
+        const dataUrl = await domtoimage.toJpeg(bioAgeComponent, { bgcolor: 'white' });
+
+        // Convert data URI to blob
+        const dataURItoBlob = (dataURI) => {
+          const byteString = atob(dataURI.split(',')[1]);
+          const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+          const uint8Array = new Uint8Array(byteString.length);
+          for (let i = 0; i < byteString.length; i++) {
+            uint8Array[i] = byteString.charCodeAt(i);
+          }
+          return new Blob([uint8Array], { type: mimeString });
+        };
+
+        const blob = dataURItoBlob(dataUrl);
+        const shareFile = new File([blob], 'biological-age.jpeg', { type: 'image/jpeg' });
+
+        const shareData = {
+          title: 'Biological Age Test',
+          text: 'This is a test of the Biological Age Chart component. Visit: https://example.com\ to find out yours.',
+          files: [shareFile],
+        };
+
+        if (navigator.canShare && navigator.canShare(shareData)) {
+          try {
+            await navigator.share(shareData);
+          } catch (shareError) {
+            console.error('Error sharing BioAge component:', shareError);
+          }
+        } else {
+          // Fallback for browsers that don't support Web Share API
+          const downloadLink = document.createElement('a');
+          downloadLink.href = dataUrl;
+          downloadLink.download = 'biological-age.jpeg';
+          downloadLink.style.display = 'none';
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+          document.body.removeChild(downloadLink);
+          alert('Chart only downloadable for device. Please check downloads folder');
+        }
+      } catch (error) {
+        console.error('Error capturing BioAge component:', error);
+        setShareError('Error capturing the chart image.');
+      } finally {
+        setSharing(false);
+      }
+    } else {
+      setShareError('Error: The BioAge component could not be found.');
+      setSharing(false);
+    }
+  };
+
+  const handleDownloadChart = async () => {
+    const bioAgeComponent = document.querySelector('.bio-age-container');
+
+    if (bioAgeComponent) {
+      try {
+        const dataUrl = await domtoimage.toJpeg(bioAgeComponent, { bgcolor: 'white' });
+
+        const downloadLink = document.createElement('a');
+        downloadLink.href = dataUrl;
+        downloadLink.download = 'biological-age.jpeg';
+        downloadLink.style.display = 'none';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+        alert('Chart image downloaded successfully.');
+      } catch (error) {
+        console.error('Error capturing BioAge component:', error);
+        setShareError('Error capturing the chart image.');
+      }
+    } else {
+      setShareError('Error: The BioAge component could not be found.');
+    }
+  };
+
+
+
+  return (
+      <div>
+        <div className="bio-age-container">
+          <BioAge />
+        </div>
+        {shareError && <div className="error">{shareError}</div>}
+        <button onClick={handleShareChart} disabled={sharing}>
+          {sharing ? 'Sharing...' : 'Share'}
+        </button>
+        <button onClick={handleDownloadChart} disabled={sharing}>
+          Download
+        </button>
+        {shareError && <div className="error">{shareError}</div>}
+      </div>
+  );
+}
+
+export default ShareBioAge;
